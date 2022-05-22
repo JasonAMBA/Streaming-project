58db608040921021349b9f631c6451f11ae999e8
Jason Amba <jason.a@fullrama.net>
2022-05-22 13:47:22 +0200

    [UPDATE] Functional registration and login

---
+++ login/login.js
@@ -0,0 +1,23 @@
+$("#connection").click((e) => {
+    e.preventDefault();
+
+    const pseudo = $("#pseudo").val();
+    const pwd = $("#pwd").val();
+
+    $.ajax({
+        url: "login.php",
+        type: "POST",
+        data: {
+            pseudo,
+            pwd
+        },
+        dataType: "json",
+        success: (res, status) => {
+            if (res.success) {
+                localStorage.setItem('user', JSON.stringify(res.user));
+                const test = JSON.parse(localStorage.getItem('user'));
+                window.location.replace("../homepage/homepage.html");
+            } else alert("Login ou mot de passe incorrect")
+        }
+    })
+})
---
+++ login/login.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+
+require_once("../utils/streamingdb.php");
+require("../utils/function.php");
+
+if (!isset($_POST['pseudo'], $_POST['pwd'])) {
+    echo json_encode(['success' => false]);
+    die();
+}
+
+$res = $pdo->query("SELECT id_user, passwd, firstname, lastname, is_admin FROM user WHERE login = '{$_POST['pseudo']}'");
+$user = resultAsArray($res)[0];
+
+if (password_verify($_POST['pwd'], $user['passwd'])) {
+    $_SESSION['connected'] = true;
+    $_SESSION['id_user'] = $user['id_user'];
+
+    echo json_encode(['success' => true, 'user' => ['firstname' => $user['firstname'], 'lastname' => $user['lastname'], 'is_admin' => $user['is_admin']]]);
+} else {
+    echo json_encode(['success' => false]);
+}
+
+?>
