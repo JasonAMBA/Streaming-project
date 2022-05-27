577b130f2b52985e2985609cdf0c5b9800d0b406
Jason Amba <jason.a@fullrama.net>
2022-05-27 10:32:29 +0200

    [UPDATE] edit arc page functional

---
+++ edit_arcs/edit_arcs.js
@@ -0,0 +1,108 @@
+$.ajax({
+    url: "../edit_mangas/edit_mangas.php",
+    type: "GET",
+    data: {
+        choice: "select"
+    },
+    dataType: "json",
+    success: (res, status) => {
+        if (res.success) {
+            let AllManga;
+
+            res.mangas.forEach(manga => {
+                AllManga += "<option value ='" + manga.id_manga + "'>" + manga.logo + "</option>";
+            });
+
+            $('#manga').append(AllManga);
+        }
+    }
+})
+
+$.ajax({
+    url: "edit_arcs.php",
+    type: "GET",
+    data: {
+        choice: 'select'
+    },
+    dataType: "json",
+    success: (res, status) => {
+        if (res.success) {
+            let html = '';
+
+            res.arcs.forEach(arc => {
+                html += "<tr id='tr-" + arc.id_arc + "'>" +
+                    "<td>" + arc.arc_number + "</td>" +
+                    "<td>" + arc.name + "</td>" +
+                    "<td>" + "<img src='" + "../mangas/" + arc.picture + "'>" + "</td>" +
+                    "<td>" + "<img src='" + "../mangas/" + arc.manga + "'>" + "</td>" +
+                    "<td><button onclick='wantToDelete(" + arc.id_arc + ", \"" + arc.name + "\")'>Supprimer</button></td>" +
+                    "</tr>";
+            });
+
+            $('table').append(html);
+        } else $("#error").html(res.msg)
+    }
+})
+
+$("input:submit").click((e) => {
+    e.preventDefault();
+
+    const id = $("#id_arc").val();
+    const arc_number = $("#arc_number").val();
+    const name = $("#name").val();
+    const picture = $("#picture").val();
+    const manga = $("#manga").val();
+
+    insertArc(arc_number, name, picture, manga);
+});
+
+function insertArc(arc_number, name, picture, manga) {
+    $.ajax({
+        url: "edit_arcs.php",
+        type: "POST",
+        data: {
+            choice: 'insert',
+            arc_number,
+            name,
+            picture,
+            manga
+        },
+        dataType:"json",
+        success: (res, status) => {
+            if (res.success) {
+                const newArc = "<tr id='tr-" + res.newid + "'>" +
+                    "<td>" + arc_number + "</td>" +
+                    "<td>" + name + "</td>" +
+                    "<td><img src='" + picture + "'></td>" +
+                    "<td><img src='" + manga + "'></td>" +
+                    "<td><button onclick='wantToDelete(" + res.newid + ", " + arc.name + ")'>Supprimer</button></td>" +
+                    "</tr>";
+
+                $('table').append(newArc);
+            } else $("#error").html(res.msg);
+
+            document.querySelector('form').reset();
+        }
+    })
+};
+
+function wantToDelete(id, name) {
+    const wantTo = confirm("Supprimer" + name + "?");
+
+    if (wantTo) {
+        $.ajax({
+            url: "edit_arcs.php",
+            type: "POST",
+            data: {
+                choice: 'delete',
+                id
+            },
+            dataType: "json",
+            success: (res, status) => {
+                if (res.success) {
+                    $("#tr-" + id).remove();
+                } else $("#error").html(res.msg);
+            }
+        });
+    }
+}
---
+++ edit_arcs/edit_arcs.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once("../../Streaming-project/utils/streamingdb.php");
+require("../../Streaming-project/utils/function.php");
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") $method = $_POST;
+else $method = $_GET;
+
+switch ($method['choice']) {
+    case 'select':
+        $res = $pdo->query("SELECT arc.*, manga.logo AS manga FROM arc INNER JOIN manga on manga.id_manga = arc.id_manga GROUP BY arc.id_arc");
+        $arcs = resultAsArray($res);
+
+        echo json_encode(["success" => true, "arcs" => $arcs]);
+        break;
+
+    case 'insert':
+        if (isset($method['arc_number'], $method['name'], $method['picture'], $method['manga']) && trim($method['arc_number']) != '' && trim($method['name']) != '' && trim($method['picture']) != ''&& trim($method['manga']) != '') {
+
+            $sql = "INSERT INTO arc (arc_number, name, picture, id_manga) VALUES (:arc_number, :name, :picture, :manga)"; 
+            $req = $pdo->prepare($sql);
+            $req->bindValue(':arc_number', $method['arc_number']);
+            $req->bindValue(':name', $method['name']);
+            $req->bindValue(':picture', $method['picture']);
+            $req->bindValue(':manga', $method['manga']);
+            $req->execute();
+
+            echo json_encode(["success" => true, "newid" => $pdo->insert_id]);
+        } else echo json_encode(["success" => false, "msg" => "Toutes les données n'ont pas été transmises"]);
+        break;
+
+    case 'delete':
+        if (isset($method['id'])) {
+            $sql = "DELETE FROM arc WHERE id_arc = {$method['id']}";
+            $pdo->query($sql);
+            echo json_encode(["success" => true]);
+        } else echo json_encode(["success" => false, "msg" => "Erreur lors de la suppression"]);
+        break;
+
+    default:
+        echo json_encode(["success" => false, "msg" => "Mauvais choix de requête"]);
+        break;
+}
+
+?>
